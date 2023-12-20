6d52fc610d67c73b321c13642c40e4ebd576781d
Tal Ben Adon <talbenadon2@gmail.com>
2023-12-20 16:31:35 +0200

    20.12.2023

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,176 +1,27 @@
 import React from "react"
 import ItemList from "./ItemList"
-import Menu from "./Menu"
-import { useState } from "react"
-import Item from "./Item"
-
-const fruits = [
-    {
-        "id": "a1",
-        "name": "Apple",
-        "color": "Red",
-        "emoji": "🍎",
-        "price": 1.99
-    },
-    {
-        "id": "a2",
-        "name": "Banana",
-        "color": "Yellow",
-        "emoji": "🍌",
-        "price": 0.99
-    },
-    {
-        "id": "a3",
-        "name": "Orange",
-        "color": "Orange",
-        "emoji": "🍊",
-        "price": 2.49
-    },
-    {
-        "id": "a4",
-        "name": "Grapes",
-        "color": "Purple",
-        "emoji": "🍇",
-        "price": 3.99
-    },
-    {
-        "id": "a5",
-        "name": "Strawberry",
-        "color": "Red",
-        "emoji": "🍓",
-        "price": 2.79
-    },
-    {
-        "id": "a7",
-        "name": "Watermelon",
-        "color": "Green",
-        "emoji": "🍉",
-        "price": 5.99
-    },
-    {
-        "id": "a8",
-        "name": "Pineapple",
-        "color": "Yellow",
-        "emoji": "🍍",
-        "price": 3.89
-    },
-    {
-        "id": "a9",
-        "name": "Mango",
-        "color": "Orange",
-        "emoji": "🥭",
-        "price": 2.99
-    },
-    {
-        "id": "a10",
-        "name": "Kiwi",
-        "color": "Brown",
-        "emoji": "🥝",
-        "price": 1.79
-    },
-    {
-        "id": "a11",
-        "name": "Peach",
-        "color": "Orange",
-        "emoji": "🍑",
-        "price": 2.29
-    },
-    {
-        "id": "a12",
-        "name": "Cherry",
-        "color": "Red",
-        "emoji": "🍒",
-        "price": 4.99
-    },
-    {
-        "id": "a13",
-        "name": "Pear",
-        "color": "Green",
-        "emoji": "🍐",
-        "price": 1.89
-    },
-    {
-        "id": "a14",
-        "name": "Plum",
-        "color": "Purple",
-        "emoji": "🍑",
-        "price": 2.69
-    },
-    {
-        "id": "a15",
-        "name": "Raspberry",
-        "color": "Red",
-        "emoji": "🍇",
-        "price": 3.49
-    },
-    {
-        "id": "a16",
-        "name": "Lemon",
-        "color": "Yellow",
-        "emoji": "🍋",
-        "price": 1.49
-    },
-    {
-        "id": "a17",
-        "name": "Cranberry",
-        "color": "Red",
-        "emoji": "🍒",
-        "price": 3.79
-    },
-    {
-        "id": "a18",
-        "name": "Apricot",
-        "color": "Orange",
-        "emoji": "🍑",
-        "price": 2.19
-    },
-    {
-        "id": "a19",
-        "name": "Avocado",
-        "color": "Green",
-        "emoji": "🥑",
-        "price": 4.99
-    },
-    {
-        "id": "a20",
-        "name": "Coconut",
-        "color": "Brown",
-        "emoji": "🥥",
-        "price": 5.49
-    }
-]
 
 
 
-const colors = []
-
-fruits.forEach((fruit => {
-    if (!colors.includes(fruit.color)) {
-        colors.push(fruit.color)
-    }
-}))
 
 
 
-export default function Content(props) {
-    const [fruitsList, setFiltered] = useState(fruits)
-    const handleColor = (color) => {
-        (color) ? setFiltered(fruits.filter(fruit => fruit.color === color)) : setFiltered(fruits)
-    }
+
+export default function Content() {
 
 
-    const handleInput = (input) => {
 
-        console.log(input);
-        (input) ? setFiltered(fruits.filter(fruit => fruit.name.toLowerCase().includes(input))) : setFiltered(fruits)
-    }
+
+
+
+
 
 
     return (
         <div>
 
-            <Menu colors={colors} handleColor={handleColor} handleInput={handleInput} />
-            <ItemList fruitsList={fruitsList} setCart={props.setCart} cart={props.cart} addToCart={props.addToCart} />
+
+            <ItemList />
         </div>
     )
 }
--- src/Item.jsx
+++ src/Item.jsx
@@ -1,47 +1,68 @@
 import React from 'react'
+import DataContext from './context/DataContext'
+import { useContext } from 'react'
 
 
+export default function Item({ item }) {
+    let { name, emoji, price, id } = item
+    // const valueFromContext = {cart, setCart}
+    const { cart, setCart } = useContext(DataContext)
 
-export default function Item({ cart, setCart, fruit }) {
+    const handlePlus = () => {
+        let newCart = { ...cart }
 
-    const addToCart = () => {
+        //if exists
+        if (newCart[id]) {
+            newCart[id].amount += 1
+        } else {
+            //creates location of id in the newCart object and then we tell it it's value
+            newCart[id] = { ...item, amount: 1 }
+        }
+        return setCart(newCart)
+    }
 
-        const inCart = cart.find(item => item.id === fruit.id)
-        if (!inCart) {
-            const newCartItem = { ...fruit, amount: 1 }
-            const newCart = [...cart, newCartItem]
+    const handleMinus = () => {
+        if (cart[id]) {
+            let newCart = { ...cart }
+            if (newCart[id].amount > 1) {
+
+                newCart[id].amount -= 1
+            } else {
+                delete newCart[id]
+            }
             setCart(newCart)
-        } else {
-            inCart.amount++
-            setCart([...cart])
-        }
-
-    }
-
-
-    const ifInCart = () => {
-        const inCart = cart.find(item => item.id === fruit.id)
-        if (!inCart) {
-            return <button onClick={addToCart}>Add To Cart</button>
-        } else {
-            return <button onClick={addToCart}>+</button>
         }
     }
+
+
+
 
 
 
     return (
 
         <div className='fruit'>
-            <div>{fruit.name}</div>
-            <div>{fruit.emoji}</div>
-            <div>{fruit.price}</div>
+            <div>{name}</div>
+            <div>{emoji}</div>
+            <div>{price}</div>
             <div>
-                {ifInCart()}
+                {cart[id]?.amount >= 1 ? <div><button onClick={handlePlus}>+</button>
+                    <span>{cart[id]?.amount || 0}</span>
+                    <button onClick={handleMinus}>-</button>
+                </div> : <button onClick={handlePlus}>Add To Cart</button>}
+
+
             </div>
 
-            {/* <button onClick={addToCart}>+</button> */}
+
 
         </div>
     )
 }
+
+
+// Object.keys
+// Object.values
+// Object.entries
+
+// take the cart, in cartList map it to cartItem
